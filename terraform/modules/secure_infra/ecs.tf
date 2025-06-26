146eed5712b73099f46dbacabed927a36b80b2d2
guyalo <guyalo@post.bgu.ac.il>
2025-06-26 13:22:42 +0300

    update sg

--- terraform/modules/secure_infra/ecs.tf
+++ terraform/modules/secure_infra/ecs.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "ecs_tasks" {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
